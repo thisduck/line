0f45d9b2f401aa8a66d36f03991e870bd906dd30
Adnan Ali <adnan.ali@gmail.com>
2015-10-05 07:06:35 -0400

    back to 1500

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -48,13 +48,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-#   version :size_3000 do
-#     process resize_to_fit: [3000,3000]
-#   end
+  version :size_3000 do
+    process resize_to_fit: [3000,3000]
+  end
 
-#   version :size_2000 do
-#     process resize_to_fit: [2000,2000]
-#   end
+  version :size_2000 do
+    process resize_to_fit: [2000,2000]
+  end
 
   version :size_1500 do
     process resize_to_fit: [1500,1500]
